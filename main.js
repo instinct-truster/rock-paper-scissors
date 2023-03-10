083f95ad3f579667fd001e92f96d1da628cb59ce
Lance Turbes <instinct_truster@protonmail.com>
2023-03-10 14:02:37 -0800

    Create rock paper scissors game

---
+++ main.js
@@ -0,0 +1,61 @@
+let playerScore = 0;
+let computerScore = 0;
+
+function getComputerChoice() {
+  let computerChoice = Math.floor(Math.random() * 3);
+  if (computerChoice === 0) {
+    return "ROCK";
+  } else if (computerChoice === 1) {
+    return "PAPER";
+  } else {
+    return "SCISSORS";
+  }
+}
+
+function playRound(playerSelection, computerSelection) {
+  const playerChoice = playerSelection.toUpperCase();
+  const computerChoice = computerSelection.toUpperCase();
+
+  if (playerChoice === computerChoice) {
+    return "You Tied!";
+  } else if (
+    (playerChoice === "ROCK" && computerChoice === "SCISSORS") ||
+    (playerChoice === "PAPER" && computerChoice === "ROCK") ||
+    (playerChoice === "SCISSORS" && computerChoice === "PAPER")
+  ) {
+    playerScore++;
+    return "You won! " + playerChoice + " beats " + computerChoice;
+  } else {
+    computerScore++;
+    return "You lost. " + computerChoice + " beats " + playerChoice;
+  }
+}
+
+function game() {
+  const computerChoice = getComputerChoice();
+  const playerChoice = prompt(
+    "Select your choice by typing out ROCK, PAPER or SCISSORS" +
+      "\n" +
+      `Player: ${playerScore} | CPU: ${computerScore}`
+  );
+  const result = playRound(playerChoice, computerChoice);
+  console.log(result);
+  console.log(playerScore);
+  console.log(computerScore);
+}
+
+for (let i = 0; i < 5; i++) {
+  game();
+}
+
+function getWinner(playerScore, computerScore) {
+  if (playerScore === computerScore) {
+    return "Tie game!";
+  } else if (playerScore > computerScore) {
+    return "You win!";
+  } else {
+    return "You lose!";
+  }
+}
+
+alert(getWinner(playerScore, computerScore));
